2d3ea7dcae4fd749bde6d80be93a8ee53e28e67d
wildyeo58 <lsolor94@gmail.com>
2019-05-18 20:44:46 -0500

    adding more components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FirstComponent from'./components/learning-examples/FirstComponent'
 
-function App() {
+class App extends Component {
+render() {
   return (
+    //JSX script ... looks like html
     <div className="App">
       My Hello World
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
+      <ThirdComponent></ThirdComponent>
     </div>
   );
+  }
 }
+
+
+  //Class Component
+class SecondComponent extends Component {
+  render() {
+    return (
+      <div className="secondComponent">
+        SecondComponent
+      </div>
+    );
+    }
+  }
+
+//function component
+  function ThirdComponent(){
+    return (
+      <div className="thirdComponent">
+        ThirdComponent
+      </div>
+    );
+  }
+  
+
 
 export default App;
---
+++ src/components/learning-examples/FirstComponent.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+export default
+
+//Class Component
+class FirstComponent extends Component {
+    render() {
+      return (
+        <div className="firstComponent">
+          FirstComponent
+        </div>
+      );
+      }
+    }
